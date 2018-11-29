f46b817d09c30f00edee10612991862b139d5426
Adson Lessa <adson.lessa@gmail.com>
2018-11-29 11:25:16 -0300

    atividade 1 e 2 OK

---
+++ resources/views/site/home/index.blade.php
@@ -0,0 +1,18 @@
+@extends('site.layouts.app')
+
+@section('content-site')
+
+<section class="slide"></section><!--Slide-->
+
+<div class="actions-form">
+    <h2>TEL LAURO DE FREITAS </h2>
+
+    
+</div><!--actions-form-->
+
+<div class="rectangle"></div><!--rectangle-->
+
+<div class="clear"></div>
+
+
+@endsection
